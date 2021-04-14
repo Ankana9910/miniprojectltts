29295dcb7687dc8da6d054d6fd0a98d101e46c1d
Ankana9910 <68809362+Ankana9910@users.noreply.github.com>
2021-04-14 17:49:13 +0530

    Add files via upload

---
+++ 3_Implementation/inc/testcal.h
@@ -0,0 +1,10 @@
+#ifdef __TESTCAL_H_
+#define __TESTCAL_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+ void calc(struct calculator c1);
+ void conv(struct converter v1);
+ 
+
+ #endif /* __TESTCAL_H_*/
